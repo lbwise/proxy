a898f0634cfd10ad0f5e93d0573556859e49269b
Liam Wise <wiseliam7@gmail.com>
2023-05-23 16:05:55 +1000

    Remove bugs

--- main.go
+++ main.go
@@ -4,32 +4,32 @@ import (
 	"net/http"
 	"fmt"
 	"io"
+	"os"
 )
 
 func main() {
+	PORT := os.Getenv("PORT")
 	server := http.NewServeMux()
 	server.HandleFunc("/", handleProxy)
-	http.ListenAndServe(":8080", server)
+	http.ListenAndServe(fmt.Sprintf(":%s", PORT), server)
 }
 
 func handleProxy(wr http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
 	method := r.URL.Query().Get("method")
-	fmt.Println(method, url)
-	req, err := http.NewRequest(method, url, r.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+
+	req, _ := http.NewRequest(method, url, r.Body)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		wr.WriteHeader(500)
 		wr.Write([]byte("An error occured making your request"))
 	}
-	fmt.Println(res)
+
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
 		wr.WriteHeader(500)
 		wr.Write([]byte("An error occured reading your response"))
 	}
+
     wr.Write(resBody)
 }
